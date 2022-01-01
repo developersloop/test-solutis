c1a60235fcb4a856095c1f9223a8fad8307d4201
developersloop <loopinfinite.all@gmail.com>
2022-01-01 08:04:31 -0300

    Configure node server

---
+++ server.js
@@ -0,0 +1,12 @@
+/* server.js */
+var app = require('express')();
+var http = require('http').Server(app);
+var serveStatic = require('serve-static');
+
+// Serve all files of the root directory
+app.use(serveStatic('.', {'index': ['index.html']}));
+
+// Listen on port 3000
+http.listen(3000, function(){
+  console.log('listening on 0.0.0.0:3000');
+});
