262071c312afb4d1d308389a4dae1948d37e0e46
developersloop <loopinfinite.all@gmail.com>
2022-01-01 08:11:30 -0300

    update gitignore

---
+++ env.js
@@ -0,0 +1,9 @@
+(function (window) {
+	window.__env = window.__env || {};
+	window.__env.apiUrl = 'https://gateway.marvel.com/v1/public';
+	window.__env.ts = 1;
+	window.__env.apiKey = 'cbb3521ea18336db7aebe6f7bec7a535';
+	window.__env.hash = '4812d3ea5046d9edf56861dc776f8c93';
+	window.__env.baseUrl = '/';
+	window.__env.enableDebug = true;
+}(this));
